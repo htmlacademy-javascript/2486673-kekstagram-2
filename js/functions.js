00844d35246446448852a090393e2cb1eaf4b452
EgorA41 <egor8619@mail.ru>
2025-01-23 15:31:55 +0700

    добавляет задание функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,34 @@ const extractNumber = (string = '') => {
 };
 
 extractNumber('qwerty123');
+
+// Задание "функции возвращаются"
+
+// переводим полученное значение часов в минуты, минуты приводим к числу. складываем часы с минутами.
+
+const getTime = (time) => {
+  const timeArray = time.split(':').map((string) => +string);
+  const timeInMinutes = (timeArray[0] * 60) + timeArray[1];
+  return timeInMinutes;
+};
+
+const isInWorkingTime = function (workStart, workEnd, meetingStart, meetingTime) {
+  const workStartInMinute = getTime(workStart);
+  const workEndInMinute = getTime(workEnd);
+  const meetingStartInMinute = getTime(meetingStart);
+  const meetingEnd = meetingStartInMinute + Number(meetingTime);
+
+  if (workStartInMinute > meetingStartInMinute || workEndInMinute < meetingStartInMinute) {
+    return false;
+  }
+  if (meetingEnd > workEndInMinute) {
+    return false;
+  }
+  return true;
+};
+
+isInWorkingTime('8:00', '17:30', '14:00', 90); // true
+isInWorkingTime('8:0', '10:0', '8:0', 120); // true
+isInWorkingTime('08:00', '14:30', '14:00', 90); // false
+isInWorkingTime('14:00', '17:30', '08:0', 90); // false
+isInWorkingTime('8:00', '17:30', '08:00', 900); // false
