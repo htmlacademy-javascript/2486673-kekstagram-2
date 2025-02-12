9edb3a77e53fff2caca455fc73ebe7ae3428bb2c
EgorA41 <egor8619@mail.ru>
2025-02-12 12:40:05 +0700

    добавляет секцию комментариев и классы для скрытия блоков

--- js/gallery.js
+++ js/gallery.js
@@ -8,9 +8,13 @@ const previewImg = preview.querySelector('.big-picture__img img');
 const previewLikesCount = preview.querySelector('.likes-count');
 const previewShownComments = preview.querySelector('.social__comment-shown-count');
 const previewCommentsCount = preview.querySelector('.social__comment-total-count');
-const previewComments = preview.querySelector('.social__comments');
+const previewCommentsList = preview.querySelector('.social__comments');
+const previewDescription = preview.querySelector('.social__caption');
+const previewCommentsBlock = preview.querySelector('.social__comment-count');
+const previewCommentsButton = preview.querySelector('.comments-loader');
 
 const fragment = document.createDocumentFragment();
+
 let photosData = [];
 
 // создает миниатюры из шаблона
@@ -35,15 +39,38 @@ const createThumbnails = (photo) => {
   container.append(fragment);
 };
 
+// создает блок с комментариями для превью
+
+const getPreviewComments = (commentData) => {
+  const previewComment = document.createElement('li');
+  previewComment.classList.add('social__comment');
+
+  previewComment.innerHTML = `
+  <img
+    class="social__picture"
+    src="${commentData.avatar}"
+    alt="${commentData.name}"
+    width="35" height="35">
+  <p class="social__text">${commentData.message}</p>
+`;
+  return previewComment;
+};
+
 // создает превью по клику на миниатюру
 
-const createPreview = ({url, likes, comments}) => {
+const createPreview = ({url, likes, comments, description}) => {
   previewImg.src = url;
   previewLikesCount.textContent = likes;
   previewShownComments.textContent = comments.length;
   previewCommentsCount.textContent = comments.length;
-};
+  previewDescription.textContent = description;
 
+  previewCommentsList.innerHTML = '';
+
+  comments.forEach((comment) => {
+    previewCommentsList.append(getPreviewComments(comment));
+  });
+};
 
 container.addEventListener('click', (evt) => {
   const picture = evt.target.closest('.picture');
@@ -55,6 +82,11 @@ container.addEventListener('click', (evt) => {
 
   const pictureId = Number(picture.dataset.id);
   const pictureData = photosData.find((photo) => photo.id === pictureId);
+
+  previewCommentsBlock.classList.add('hidden');
+  previewCommentsButton.classList.add('hidden');
+
+  document.body.classList.add('modal-open');
 
   openPreview(pictureData);
 });
