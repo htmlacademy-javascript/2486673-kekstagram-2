c0719336e198c0f399949cd2acc726033d4faf9b
EgorA41 <egor8619@mail.ru>
2025-03-11 17:33:51 +0700

    Создает валидатор хештегов и комментов

--- js/form.js
+++ js/form.js
@@ -1,9 +1,14 @@
 import { isEscapeKey } from './util.js';
 
-const input = document.querySelector('.img-upload__input');
-const uploadForm = document.querySelector('.img-upload__overlay');
-const formCloseButton = document.querySelector('.img-upload__cancel');
+const form = document.querySelector('.img-upload__form');
+const input = form.querySelector('.img-upload__input');
+const uploadForm = form.querySelector('.img-upload__overlay');
+const formCloseButton = form.querySelector('.img-upload__cancel');
 
+const hashtagInput = form.querySelector('.text__hashtags');
+const commentInput = form.querySelector('.text__description');
+
+let error = '';
 
 const openForm = () => {
   uploadForm.classList.remove('hidden');
@@ -15,6 +20,7 @@ const closeForm = () => {
   uploadForm.classList.add('hidden');
   document.body.classList.remove('modal-open');
   input.value = '';
+  hashtagInput.value = '';
 };
 
 input.addEventListener('change', () => {
@@ -29,3 +35,113 @@ function onFormKeydown(evt) {
 }
 
 formCloseButton.addEventListener('click', closeForm);
+
+// валидация формы. будет вынесена в отдельный модуль
+
+const ERROR_MESSAGES = {
+  tooMany: 'Нельзя использовать больше 5 хэштегов',
+  invalidFormat: 'Хэштег должен начинаться с # и содержать только буквы и цифры',
+  duplicate: 'Хэштеги не должны повторяться',
+  tooLong: 'длина комментария не должна быть больше 140 символов',
+};
+
+const HASHTAG_AMOUNT = 5;
+
+const COMMENT_LENGTH = 140;
+
+const hashtagRegex = /^#[a-zа-яё0-9]{1,19}$/i;
+
+// вынес функцию отдельно. кода стало больше. Оставлять?
+
+const checkDuplicate = (uniqueHashtags, hashtag) => {
+  if (uniqueHashtags.has(hashtag)) {
+    error = ERROR_MESSAGES.duplicate;
+    return false;
+  }
+  return true;
+};
+
+// валидация хештегов
+
+const hashtagValidate = (value) => {
+
+  const inputValue = value.trim().toLowerCase();
+  const hashtagArray = inputValue.split(/\s+/);
+
+  const uniqueHashtags = new Set();
+
+  // проверка пустого поля
+
+  if (!value) {
+    return true;
+  }
+
+  // проверка количества хештегов
+
+  if (hashtagArray.length > HASHTAG_AMOUNT) {
+    error = ERROR_MESSAGES.tooMany;
+    return false;
+  }
+
+  for (const hashtag of hashtagArray) {
+
+    // проверка допустимых символов
+
+    if (!hashtagRegex.test(hashtag)) {
+      error = ERROR_MESSAGES.invalidFormat;
+      return false;
+    }
+
+    // проверка повторов
+
+    if (!checkDuplicate(uniqueHashtags, hashtag)) {
+      return false;
+    }
+
+    // if (uniqueHashtags.has(hashtag)) {
+    //   error = ERROR_MESSAGES.duplicate;
+    //   return false;
+    // }
+
+    uniqueHashtags.add(hashtag);
+  }
+  return true;
+};
+
+
+// валидатор комментариев
+
+const commentValidate = (value) => {
+  const commentValue = value.trim();
+  if (commentValue.length > COMMENT_LENGTH) {
+    error = ERROR_MESSAGES.tooLong;
+    return false;
+  }
+  return true;
+};
+
+// настройки валидатора
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
+
+commentInput.addEventListener('input', () => {
+  pristine.validate(commentInput);
+});
+
+hashtagInput.addEventListener('input', () => {
+  pristine.validate(hashtagInput);
+});
+
+form.addEventListener('submit', (evt) => {
+  pristine.validate();
+  evt.preventDefault();
+});
+
+pristine.addValidator(hashtagInput, hashtagValidate, () => error);
+
+pristine.addValidator(commentInput, commentValidate, () => error);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import { getPhotos } from './photo-description.js';
 import { initGallery } from './gallery.js';
 
+import './form.js';
+
 initGallery(getPhotos());
